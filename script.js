7265eb016ccecf130b0cc7f9450426f43851fb64
emanuel colpo wendpap <102667137+Atanab01@users.noreply.github.com>
2024-07-24 10:50:14 -0300

    ..

--- script.js
+++ script.js
@@ -1,40 +1,42 @@
-[10:24, 7/24/2024] Erik Albert: // Mensagem de boss-vindas ao clicar no botão document.addEventListener('DOMContentLoaded', () => {
+// Mensagem de boas-vindas ao clicar no botão document.addEventListener('DOMContentLoaded', () => { const button = document.querySelector('button');
 
-    const button = document.querySelector('button');
-    
-    button.addEventListener('click', () => {
-    
-    alert('Estamos animados para começar o ano com vocês!');
-    
-    });
-    
-    // Contador de dias para o início das aulas
-    [10:25, 7/24/2024] Erik Albert: const startDate=new Date('2024-08-01);// Defina a data de inicio das nutas
-    
-    const today new Date();
-    
-    const timeDiff startDate-today;
-    
-    const daysLeftMath.ceil(time Diff/(1000-60*60*24));
-    
-    const countdown Element = document.createElement('p');
-    
-    countdownElement.textContent = Faltam ${daysLeft) dias para o início das aulas!": document.querySelector('.card").appendChild(countdownElement);
-    
-    // Alterar a imagem ao clicar
-    
-    const image = document.querySelector('img'); image.addEventListener('click', () => { image.src=voltaaulas.jpg; });
-    
-    // Mostrar uma mensagem de boas-vindas com animação
-    
-    const card document.querySelector('.card');
-    
-    card.style.opacity
-    
-    card.style.transition = 'opacity 2s';
-    
-    setTimeout(() => {
-    
-    card.style.opacity= 1; ), 100);
-    
-    });
+button.addEventListener('click', () => {
+
+alert('Estamos animados para começar o ano com vocês!'); });
+
+// Contador de dias para o início das aulas
+const startDate = new Date('2024-08-01'); // Defina a data de início das aulas
+
+const today new Date();
+
+const timeDiff start Date-today;
+
+const daysLeft Math.ceil(timeDiff/(1000*60*60*24));
+
+const countdown Element = document.createElement('p');
+
+countdownElement.textContent = 'Faltam ${daysLeft) dias para o início das aulas!"; document.querySelector(.card").appendChild(countdown Element);
+
+// Alterar a imagem ao clicar
+
+const image document.querySelector('img');
+
+image.addEventListener('click', () => {
+
+image.src voltaaulas.jpg:
+
+});
+
+// Mostrar uma mensagem de boas-vindas com animação
+
+const card document.querySelector('.card');
+
+card.style.opacity = 0;
+
+card.style.transition = 'opacity 2s';
+
+setTimeout(() => {
+
+card.style.opacity = 1; }, 100);
+
+});
